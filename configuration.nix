ea895cd137f70421858853a12fdcd9a92b372da6
phaon1 <85368222+phaon1@users.noreply.github.com>
2023-09-14 15:35:31 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 let
@@ -16,157 +12,133 @@ in
       aagl-gtk-on-nix.module
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+# Bootloader.
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+networking.hostName = "nixos"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+# Enable networking
+networking.networkmanager.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+# Set your time zone.
+time.timeZone = "Europe/London";
 
-  # Set your time zone.
-  time.timeZone = "Europe/London";
+# Select internationalisation properties.
+i18n.defaultLocale = "en_GB.UTF-8";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
+i18n.extraLocaleSettings = {
+  LC_ADDRESS = "en_GB.UTF-8";
+  LC_IDENTIFICATION = "en_GB.UTF-8";
+  LC_MEASUREMENT = "en_GB.UTF-8";
+  LC_MONETARY = "en_GB.UTF-8";
+  LC_NAME = "en_GB.UTF-8";
+  LC_NUMERIC = "en_GB.UTF-8";
+  LC_PAPER = "en_GB.UTF-8";
+  LC_TELEPHONE = "en_GB.UTF-8";
+  LC_TIME = "en_GB.UTF-8";
+};
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
+# Enable the X11 windowing system.
+services.xserver.enable = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+# Enable the KDE Plasma Desktop Environment.
+services.xserver.displayManager.sddm.enable = true;
+services.xserver.desktopManager.plasma5.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+# Configure keymap in X11
+services.xserver = {
+  layout = "us";
+  xkbVariant = "";
+};
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+# Enable CUPS to print documents.
+services.printing.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+# Enable sound with pipewire.
+sound.enable = true;
+hardware.pulseaudio.enable = false;
+security.rtkit.enable = true;
+services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+};
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.noah = {
-    isNormalUser = true;
-    description = "noah";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-      discord
-      spotify
-      podman
-      distrobox
-      git
-      protonup-qt
-      wine
-    ];
-  };
-
-  home-manager.users.noah = { pkgs, ... }: {
-    home.packages = [];
-    programs.zsh = {
-      enable = true;
-        oh-my-zsh = {
-          enable = true;
-          theme = "agnoster";
-     };
-
-     zplug = {
-       enable = true;
-         plugins = [
-           { name = "zsh-users/zsh-autosuggestions"; }
-           { name = "zsh-users/zsh-syntax-highlighting"; }
-         ];    
-     }; 
-    };
-    
-    home.stateVersion = "23.05";
-  };
-
-  # Enable zsh
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-
-  # Enable dconf
-  programs.dconf.enable = true;
-
-  #Enable virtualisation
-  virtualisation.libvirtd.enable = true;
-
-  # Enable nur
-  nixpkgs.config.packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      inherit pkgs;
-    };
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  #Enable flatpak
-  services.flatpak.enable = true;
-
-  # Enable flakes and nix command
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  #Enable aagl
-  programs.anime-game-launcher.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-   virt-manager
-   pkgs.libsForQt5.kate
+# Define a user account. Don't forget to set a password with ‘passwd’.
+users.users.noah = {
+  isNormalUser = true;
+  description = "noah";
+  extraGroups = [ "networkmanager" "wheel" ];
+  packages = with pkgs; [
+    firefox
+    discord
+    spotify
+    podman
+    distrobox
+    git
+    protonup-qt
+    wine
   ];
+};
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+home-manager.users.noah = { pkgs, ... }: {
+  home.packages = [];
+  programs.zsh = {
+    enable = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "agnoster";
+    };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+    zplug = {
+      enable = true;
+        plugins = [
+          { name = "zsh-users/zsh-autosuggestions"; }
+          { name = "zsh-users/zsh-syntax-highlighting"; }
+        ];    
+    }; 
+  };
+    
+  home.stateVersion = "23.05";
+};
+
+# Enable zsh
+programs.zsh.enable = true;
+users.defaultUserShell = pkgs.zsh;
+
+# Enable dconf
+programs.dconf.enable = true;
+
+#Enable virtualisation
+virtualisation.libvirtd.enable = true;
+
+# Enable nur
+nixpkgs.config.packageOverrides = pkgs: {
+  nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+    inherit pkgs;
+  };
+};
+
+# Allow unfree packages
+nixpkgs.config.allowUnfree = true;
+
+#Enable flatpak
+services.flatpak.enable = true;
+
+# Enable flakes and nix command
+nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+#Enable aagl
+programs.anime-game-launcher.enable = true;
+
+environment.systemPackages = with pkgs; [
+  virt-manager
+  pkgs.libsForQt5.kate
+];
+
+system.stateVersion = "23.05";
 
 }
